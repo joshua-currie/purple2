4712b2e17fe6d8c0d27a40db35e10e31d4bb0f10
Joshua Currie <joshuacurrie58@gmail.com>
2024-09-29 10:36:21 -0400

    fix a file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,  // Enable React's Strict Mode for better debugging
-    experimental: {
-      appDir: true,  // Enable the App Router
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
